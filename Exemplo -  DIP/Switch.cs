f2e2526c9704f42e79318d877fe044ccf85a9ef9
JoaoGarbin <garbinjoaovitor@gmail.com>
2025-06-12 20:25:06 -0300

    Adicionar arquivos de projeto.

---
+++ Exemplo -  DIP/Switch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo____DIP
+{
+    public class Switch
+    {
+        private Lamp lamp;
+        public Switch(Lamp device)
+        {
+            this.lamp = device;
+        }
+        public void Press()
+        {
+            lamp.Operate();
+        }
+    }
+}
